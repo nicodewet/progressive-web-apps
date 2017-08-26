42e13c97e59bf3bd0d53edc16d23ceb4d1a5d144
Nico de Wet <nico@nicodewet.com>
2017-08-26 13:38:35 +1200

    chache new requests cumultivelt, if req is not in cache make the original req and add resp to cache

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 This repository represents my executable learnings from the [Progressive Web Apps](https://www.manning.com/books/progressive-web-apps) book.
 
-In general I want to run examples with Node.js v6.11.1 (which is what I happen to have on my local right now), Express 4 and ideally I want to wrap them with docker and deploy them to my domain (makario.io).
+In general I want to run examples with Node.js v8.4.0 (which is what I happen to have on my local right now), Express 4 and ideally I want to wrap them with docker and deploy them to my domain (makario.io).
 
 In terms of bookmarks, if you bought the book you may know the Service Workers are key, so here are some relevant bookmarks and as you may expect MDN has **excellent** documentation:
 
 * [MDN - Service Worker API](https://developer.mozilla.org/en/docs/Web/API/Service_Worker_API)
 * [Browser Servicer Worker Ready?](https://jakearchibald.github.io/isserviceworkerready/)
+* [Service Workers: an Introduction](https://developers.google.com/web/fundamentals/getting-started/primers/service-workers)
--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,60 @@
 var cacheName = 'helloWorld';
 
-// Tap into the Service Worker installation event
+// Tap into the Service Worker installation event.
+// This is known as "pre-caching".
 self.addEventListener('install', event => {
+  // https://developer.mozilla.org/en-US/docs/Web/API/ExtendableEvent/waitUntil
   event.waitUntil(
     // Open the specified cache
     caches.open(cacheName)
-    // Add the JavaScript and image into the cache
-    .then(cache => cache.addAll([
-      './js/script.js',
-      './images/hello.png'
-    ]))
+      // Add the JavaScript and image into the cache
+      .then(cache => cache.addAll([
+        './js/script.js',
+        './images/hello.png'
+      ]))
   );
 });
 
-self.addEventListener('fetch', function(event) {
+// Read assets from our primed cache ...
+self.addEventListener('fetch', function (event) {
   event.respondWith(
+    // Check if an incoming URL matches anything in our cache
     caches.match(event.request)
-    .then(function(response) {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
+      .then(function (response) {
+        if (response) {
+          // we have a match in our cache so return the resource from the cache
+          return response;
+        }
+
+        // clone the request since request is a stream and can only be
+        // consumed once
+        var fetchRequest = event.request.clone();
+
+        // resource doesn't exist in cache so fetch the requested resource
+        // by trying to make the original HTTP request as intended
+        // https://developer.mozilla.org/en/docs/Web/API/Fetch_API
+        return fetch(fetchRequest).then(
+          function (fetchResponse) {
+            if (!fetchResponse || fetchResponse.status !== 200) {
+              // if the request fails or the server returns an error code return 
+              // the error immediately
+              return fetchResponse;
+            }
+
+            // clone the response because we need to both add it into the cache AND 
+            // because it is used as the final returned response
+            var responseToCache = fetchResponse.clone();
+
+            // open our helloWorld cache
+            caches.open(cacheName)
+              .then(function (cache) {
+                // add the response into the cache
+                cache.put(event.request, responseToCache);
+              });
+
+            return fetchResponse;
+          }
+        );
+      })
   );
 });
